eed0896ac922dbc5c34ab93f666ff22a2fb3669c
Tomoki sato <s22C1057KW@s.chibakoudai.jp>
2023-10-26 13:05:51 +0900

    at test.bash

---
+++ test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Tomoki sato
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng() {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] $$ echo OK
+exit $res
