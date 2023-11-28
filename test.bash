0c27a4d27ea04554104804725c226a0a488ec5a0
Tomoki sato <s22C1057KW@s.chibakoudai.jp>
2023-11-28 23:16:27 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Tomoki sato
 # SPDX-Licence-Identifier: BSD-3-Clause
 
@@ -10,8 +10,56 @@ ng () {
 res=0
 
 ### I/O TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15.0 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
+  out=$(echo | ./plus) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+### I/O TEST ###
+out=$(seq 5 | ./sub)
+[ "${out}" = -15.0 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+  out=$(echo あ | ./sub)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　
+  out=$(echo | ./sub) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+### I/O TEST ###
+out=$(seq 5 | ./multi)
+[ "${out}" = 120.0 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+  out=$(echo あ | ./multi)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　
+  out=$(echo | ./multi) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+### I/O TEST ###
+out=$(seq 5 | ./dev)
+[ "${out}" = 0.008333333333333333 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+  out=$(echo あ | ./dev)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　
+  out=$(echo | ./dev) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
