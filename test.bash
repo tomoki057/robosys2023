d4435e478e2d98109f57787e2571d783a7a0dfad
Tomoki sato <s22C1057KW@s.chibakoudai.jp>
2023-11-28 17:14:54 +0900

    test.bash

--- test.bash
+++ test.bash
@@ -11,7 +11,10 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
+[ "${out}" = "15.0
+-15.0
+120.0
+0.008333333333333333" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
